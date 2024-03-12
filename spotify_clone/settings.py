d62c546907c9246997c3c9964a8e6b3525e85b72
Boluex <awomewekolawole1@gmail.com>
2024-03-12 10:38:37 +0100

    New message

--- spotify_clone/settings.py
+++ spotify_clone/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'spotify_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'YOUR-NAME',
-        'USER': 'YOUR-USER',
-        'PASSWORD': 'PASSWORD',
-        'HOST': 'YOUR-HOST',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'YOUR-NAME',
+#         'USER': 'YOUR-USER',
+#         'PASSWORD': 'PASSWORD',
+#         'HOST': 'YOUR-HOST',
+#         'PORT': 5432
+#     }
+# }
 
 
 # Password validation
